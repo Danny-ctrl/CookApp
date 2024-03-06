494ee260d32ab94decb1fe4dbd501616b66c2852
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2024-03-06 01:39:10 -0300

    ingredient parte 1

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,20 @@
-import {View,Text} from "react-native"
+import {View,Text, ScrollView} from "react-native"
 
 import{styles} from "./styles"
+import { Ingredient } from "@/components/Ingredient"
+import {useState} from 'react'
 
 export default function Index(){
+  const [selected,setSelected] = useState<string[]>([])
+  function handleToggleSelected(value:string){
+if(selected.includes(value)){
+  return setSelected((state)=>state.filter((item)=>item !==
+  value))
+}
+
+setSelected((state)=>[...state,value])
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -10,7 +22,19 @@ export default function Index(){
        <Text style={styles.subtitle}> os produtos</Text>
         </Text>
 
-      <Text style={styles.message}>Descubra receitas baseadas nos produtos que você escolheu.</Text>
+      <Text style={styles.message}>
+        Descubra receitas baseadas nos produtos que você escolheu.
+        </Text>
+
+        <ScrollView  contentContainerStyle={styles.ingredients}
+          showsVerticalScrollIndicator={false}
+          >
+  {Array.from({length:100}).map((item,index)=>(
+
+<Ingredient key={index} name = "Tomate" image=" " selected
+onPress={()=>handleToggleSelected(String(index))}/>
+))}
+        </ScrollView>
     </View>
   )
 }
